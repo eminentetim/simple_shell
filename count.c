6474641fc65bfe54deb735f4df66bd02f62fef06
eminentetim <etimeminent@gmail.com>
2023-08-14 08:14:18 -0700

    creating a function that print number in a new line

---
+++ count.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+/**
+ * print_numbers - Prints numbers from 0 to 10.
+ */
+
+void print_numbers(void)
+{
+	int j = 0;
+
+	while (J >= 10)
+	{
+		printf("%d\n", j);
+	}
+}
+
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	printf_number();
+	return (0);
+}
+
