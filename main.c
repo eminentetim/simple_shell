d88fa0044ee0f3eb7a75f3227acf7852ad0f8a75
eminentetim <etimeminent@gmail.com>
2023-08-23 23:30:52 -0700

    promp.c

---
+++ main.c
@@ -0,0 +1,8 @@
+#include "simple.h"
+
+int main(void)
+{
+	shell_interactive();
+	return (0);
+}
+
---
+++ prompt.c
@@ -0,0 +1,25 @@
+#include "simple.h"
+
+void shell_interactive(void)
+{
+	char *line;
+	char **args;
+	int status = -1;
+
+	do
+	{
+		printf("Eminent_shell$: ");
+		line = read_line();
+		args = split_line(line);
+        	status = execute_args(args);
+        	
+		free(line);
+		free_args(args);
+        /* Exit with status*/
+		if (status >= 0)
+		{
+			exit(status);
+        	}
+	}
+	while (status == -1);
+}
